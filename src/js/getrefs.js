4650ea467250cf6bbfa9438fbcfea4333b04e4c0
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2021-11-17 23:44:26 +0200

    Added file getrefs.js and some styles

---
+++ src/js/getrefs.js
@@ -0,0 +1,9 @@
+export default function getRefs () {
+  return {
+   searchForm: document.querySelector('#search-form'),
+   gallery: document.querySelector('.gallery'),
+   loadMoreBtn: document.querySelector('.load-more'),
+   inputForm: document.querySelector('.search-form > input'),
+   searchBtn: document.querySelector('.search-form > button'),
+  }
+}
