b576be441f51fefa4fd6f8ad7615207c5f901770
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2021-11-18 15:08:03 +0200

    Added file fetchimage.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import './css/styles.css';
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios  from 'axios';
 
 import getRefs from './js/getrefs';
+import API from './js/fetchimage';
 
 
 const refs = getRefs();
 
-refs.searchBtn.addEventListener('submit', startSearchImage);
+  refs.searchForm.addEventListener('submit', startSearchImage);
+
+  function startSearchImage (event) {
+    event.preventDefault();
+    const query = event.currentTarget.elements.searchQuery.value;
+
+    API.fetchImage(query).then(console.log);
+  }
 
 
 
-function startSearchImage (event) {
-  console.log(event.currentTarget);
-  event.preventDefault();
-  const whatFind = refs.inputForm.value;
-  console.log(whatFind);
-
-}
-// fetch('https://pixabay.com/api/?key=24307616-b0118d635ae4446a17d5d0140').then(response => response.json()).then(console.log);
---
+++ src/js/fetchimage.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+const BASE_URL = 'https://pixabay.com/api/';
+
+
+
+
+
+function fetchImage(query) {
+
+  const searchParams = new URLSearchParams({
+    key: '24307616-b0118d635ae4446a17d5d0140',
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page: 40,
+  });
+
+  return axios.get(`${BASE_URL}?${searchParams}`);
+};
+
+
+export default { fetchImage };
+
+
+// Список параметров строки запроса которые тебе обязательно необходимо указать:
+
+// key - твой уникальный ключ доступа к API.
+// q - термин для поиска. То, что будет вводить пользователь.
+// image_type - тип изображения. Мы хотим только фотографии, поэтому задай значение photo.
+// orientation - ориентация фотографии. Задай значение horizontal.
+// safesearch - фильтр по возрасту. Задай значение true.
+// В ответе будет массив изображений удовлетворивших критериям параметров запроса. Каждое изображение описывается объектом, из которого тебе интересны только следующие свойства:
+
+// webformatURL - ссылка на маленькое изображение для списка карточек.
+// largeImageURL - ссылка на большое изображение.
+// tags - строка с описанием изображения. Подойдет для атрибута alt.
+// likes - количество лайков.
+// views - количество просмотров.
+// comments - количество комментариев.
+// downloads - количество загрузок.
+
